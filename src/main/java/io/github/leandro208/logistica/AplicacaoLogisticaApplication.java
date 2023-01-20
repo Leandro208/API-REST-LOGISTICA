7caf2ee1cf9822f4cfbdf0a22f218d4e7ce158a3
Leandro Barbosa <boyleandro_208@hotmail.com>
2023-01-19 23:08:56 -0300

    feat: criando projeto spring boot

---
+++ src/main/java/io/github/leandro208/logistica/AplicacaoLogisticaApplication.java
@@ -0,0 +1,13 @@
+package io.github.leandro208.logistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacaoLogisticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacaoLogisticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/leandro208/logistica/AplicacaoLogisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leandro208.logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacaoLogisticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
